390bf29b72e58112fad0006d1cb761a544011df2
bonzuz <86501621+bonzuz@users.noreply.github.com>
2023-03-21 12:45:50 -0700

    Create appleID.sh
    appleID test

---
+++ appleID.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+## Get logged in user
+consoleUser() {
+	echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }'
+}
+user=$(consoleUser)
+
+icloudaccount=$(sudo -u "$user" defaults read | grep "App Store Product Source Accounts:" | head -1 | awk '/Accounts:/ { print $6 } ' | cut -d '"' -f 1 )
+
+if [ -z "$icloudaccount" ] 
+then
+    echo "<result>No Accounts</result>"
+else
+    echo "<result>${icloudaccount}</result>"
+fi
+
